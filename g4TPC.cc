f2cfa713fc4cc6871b4738901f8a88ab74225221
Steven Green <sg1sg2sg3@hotmail.co.uk>
2019-03-20 15:24:13 +0000

    Refining the g4TPC.cc main script.

--- g4TPC.cc
+++ g4TPC.cc
@@ -48,117 +48,131 @@
 #include "G4VisExecutive.hh"
 #include "G4UIExecutive.hh"
 
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+//------------------------------------------------------------------------------
 
-namespace {
-  void PrintUsage() {
+namespace
+{
+
+void PrintUsage()
+{
     G4cerr << " Usage: " << G4endl;
     G4cerr << " exampleG4TPC [-m macro ] [-u UIsession] [-t nThreads]" << G4endl;
-    G4cerr << "   note: -t option is available only for multi-threaded mode."
-           << G4endl;
-  }
+    G4cerr << "   note: -t option is available only for multi-threaded mode." << G4endl;
 }
 
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+}
+
+//------------------------------------------------------------------------------
 
 int main(int argc,char** argv)
 {
-  // Evaluate arguments
-  //
-  if ( argc > 7 ) {
-    PrintUsage();
-    return 1;
-  }
-  
-  G4String macro;
-  G4String session;
-#ifdef G4MULTITHREADED
-  G4int nThreads = 0;
-#endif
-  for ( G4int i=1; i<argc; i=i+2 ) {
-    if      ( G4String(argv[i]) == "-m" ) macro = argv[i+1];
-    else if ( G4String(argv[i]) == "-u" ) session = argv[i+1];
-#ifdef G4MULTITHREADED
-    else if ( G4String(argv[i]) == "-t" ) {
-      nThreads = G4UIcommand::ConvertToInt(argv[i+1]);
+    // Evaluate arguments
+    if ( argc > 7 )
+    {
+        PrintUsage();
+        return 1;
     }
-#endif
-    else {
-      PrintUsage();
-      return 1;
-    }
-  }  
-  
-  // Detect interactive mode (if no macro provided) and define UI session
-  //
-  G4UIExecutive* ui = 0;
-  if ( ! macro.size() ) {
-    ui = new G4UIExecutive(argc, argv);
-  }
 
-  // Choose the Random engine
-  //
-  G4Random::setTheEngine(new CLHEP::RanecuEngine);
-  
-  // Construct the default run manager
-  //
+    G4String macro;
+    G4String session;
 #ifdef G4MULTITHREADED
-  G4MTRunManager * runManager = new G4MTRunManager;
-  if ( nThreads > 0 ) { 
-    runManager->SetNumberOfThreads(nThreads);
-  }  
+    G4int nThreads = 0;
+#endif
+
+    for ( G4int i=1; i<argc; i=i+2 )
+    {
+        if ( G4String(argv[i]) == "-m" )
+        {
+            macro = argv[i+1];
+        }
+        else if ( G4String(argv[i]) == "-u" )
+        {
+            session = argv[i+1];
+        }
+#ifdef G4MULTITHREADED
+        else if ( G4String(argv[i]) == "-t" )
+        {
+             nThreads = G4UIcommand::ConvertToInt(argv[i+1]);
+        }
+#endif
+        else
+        {
+            PrintUsage();
+            return 1;
+        }
+    }
+
+    // Detect interactive mode (if no macro provided) and define UI session
+    G4UIExecutive* pG4UIExecutive = nullptr;
+
+    if ( ! macro.size() )
+    {
+        pG4UIExecutive = new G4UIExecutive(argc, argv);
+    }
+
+    // Choose the Random engine
+    G4Random::setTheEngine(new CLHEP::RanecuEngine);
+
+    // Construct the default run manager
+#ifdef G4MULTITHREADED
+    G4MTRunManager * runManager = new G4MTRunManager;
+    if ( nThreads > 0 )
+    {
+        runManager->SetNumberOfThreads(nThreads);
+    }
 #else
-  G4RunManager * runManager = new G4RunManager;
+    G4RunManager * runManager = new G4RunManager;
 #endif
 
-  // Set mandatory initialization classes
-  //
-  G4TPCDetectorConstruction* detConstruction = new G4TPCDetectorConstruction();
-  runManager->SetUserInitialization(detConstruction);
+    // Set mandatory initialization classes
+    G4TPCDetectorConstruction* pG4TPCDetectorConstruction = new G4TPCDetectorConstruction();
+    runManager->SetUserInitialization(pG4TPCDetectorConstruction);
 
-  //G4VModularPhysicsList* physicsList = new FTFP_BERT; <- Original
-  G4VModularPhysicsList* physicsList = new QGSP_BERT;
-  physicsList->RegisterPhysics(new G4StepLimiterPhysics()); // Has to be added to limit step size
-  runManager->SetUserInitialization(physicsList);
-    
-  G4TPCActionInitialization* actionInitialization
-     = new G4TPCActionInitialization(detConstruction);
-  runManager->SetUserInitialization(actionInitialization);
-  
-  // Initialize visualization
-  //
-  G4VisManager* visManager = new G4VisExecutive;
-  // G4VisExecutive can take a verbosity argument - see /vis/verbose guidance.
-  // G4VisManager* visManager = new G4VisExecutive("Quiet");
-  visManager->Initialize();
+    //G4VModularPhysicsList* pG4VModularPhysicsList = new FTFP_BERT; <- Original
+    G4VModularPhysicsList* pG4VModularPhysicsList = new QGSP_BERT;
+    pG4VModularPhysicsList->RegisterPhysics(new G4StepLimiterPhysics()); // Has to be added to limit step size
+    runManager->SetUserInitialization(pG4VModularPhysicsList);
 
-  // Get the pointer to the User Interface manager
-  G4UImanager* UImanager = G4UImanager::GetUIpointer();
+    G4TPCActionInitialization* pG4TPCActionInitialization = new G4TPCActionInitialization(pG4TPCDetectorConstruction);
+    runManager->SetUserInitialization(pG4TPCActionInitialization);
 
-  // Process macro or start UI session
-  //
-  if ( macro.size() ) {
-    // batch mode
-    G4String command = "/control/execute ";
-    UImanager->ApplyCommand(command+macro);
-  }
-  else  {  
-    // interactive mode : define UI session
-    UImanager->ApplyCommand("/control/execute init_vis.mac");
-    if (ui->IsGUI()) {
-      UImanager->ApplyCommand("/control/execute gui.mac");
+    // Initialize visualization
+    G4VisManager* pG4VisManager = new G4VisExecutive;
+    // G4VisExecutive can take a verbosity argument - see /vis/verbose guidance.
+    // G4VisManager* pG4VisManager = new G4VisExecutive("Quiet");
+    pG4VisManager->Initialize();
+
+    // Get the pointer to the User Interface manager
+    G4UImanager* pG4UImanager = G4UImanager::GetUIpointer();
+
+    // Process macro or start UI session
+    if ( macro.size() )
+    {
+        // batch mode
+        G4String command = "/control/execute ";
+        pG4UImanager->ApplyCommand(command+macro);
     }
-    ui->SessionStart();
-    delete ui;
-  }
+    else
+    {
+        // interactive mode : define UI session
+        pG4UImanager->ApplyCommand("/control/execute init_vis.mac");
 
-  // Job termination
-  // Free the store: user actions, physics_list and detector_description are
-  // owned and deleted by the run manager, so they should not be deleted 
-  // in the main() program !
+        if (pG4UIExecutive->IsGUI())
+        {
+            pG4UImanager->ApplyCommand("/control/execute gui.mac");
+        }
 
-  delete visManager;
-  delete runManager;
+        pG4UIExecutive->SessionStart();
+        delete pG4UIExecutive;
+    }
+
+    // Job termination
+    // Free the store: user actions, physics_list and detector_description are
+    // owned and deleted by the run manager, so they should not be deleted
+    // in the main() program !
+
+    delete pG4VisManager;
+    delete runManager;
 }
 
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo.....
+//------------------------------------------------------------------------------
