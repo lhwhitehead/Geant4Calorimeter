abe01d66e4640fcd6fb85c66e7622957faaaf710
Steven Green <sg1sg2sg3@hotmail.co.uk>
2019-06-18 11:04:33 +0100

    Created the G4MCParticleUserAction class.

---
+++ include/G4MCParticleUserAction.hh
@@ -0,0 +1,63 @@
+/**
+ *  @file   include/G4MCParticleUserAction.hh
+ *
+ *  @brief  Header file for the geant4 MCParticle user action class.
+ *
+ *  $Log: $
+ */
+
+#ifndef GEANT4_MCPARTICLE_USER_ACTION_H
+#define GEANT4_MCPARTICLE_USER_ACTION_H 1
+
+#include "G4UserEventAction"
+#include "G4UserRunAction.hh"
+#include "G4UserSteppingAction"
+#include "G4UserTrackingAction"
+
+#include "MCParticle.hh"
+
+/**
+ *  @brief G4MCParticleUserAction class
+ */
+class G4MCParticleUserAction: public G4UserRunAction, public G4UserEventAction, public G4UserTrackingAction, public G4UserSteppingAction
+{
+public:
+    /**
+     *  Default constructor
+     */
+    G4MCParticleUserAction();
+
+    /**
+     *  Destructor
+     */
+    ~G4MCParticleUserAction();
+
+    /**
+     *  Functions expected from a G4UserRunAction
+     */
+    void BeginOfRunAction(const G4Run *pG4Run);
+    void EndOfRunAction(const G4Run *pG4Run);
+
+    /**
+     *  Functions expected from a G4UserEventAction
+     */
+    void BeginOfEventAction(const G4Event *pG4Event);
+    void EndOfEventAction(const G4Event *pG4Event);
+
+    /**
+     *  Functions expected from a G4UserTrackingAction
+     */
+    void PreTrackingAction(const G4Track *pG4Track);
+    void PostTrackingAction(const G4Track *pG4Track);
+
+    /**
+     *  Functions expected from a G4UserSteppingAction
+     */
+    void SteppingAction(const G4Step *pG4Step);
+
+private:
+};
+
+//------------------------------------------------------------------------------------------------------------------------------------------ 
+
+#endif // #ifndef GEANT4_MCPARTICLE_USER_ACTION_H
---
+++ src/G4MCParticleUserAction.cc
@@ -0,0 +1,62 @@
+/**
+ *  @file   src/G4MCParticleUserAction.cc
+ *
+ *  @brief  Implementation of the geant4 MCParticle user action class.
+ *
+ *  $Log: $
+ */
+
+#include "G4MCParticleUserAction.hh"
+
+G4MCParticleUserAction::G4MCParticleUserAction()
+{
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------ 
+
+G4MCParticleUserAction::~G4MCParticleUserAction()
+{
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------ 
+
+void G4MCParticleUserAction::BeginOfRunAction(const G4Run * /*pG4Run*/)
+{
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------ 
+
+void G4MCParticleUserAction::EndOfRunAction(const G4Run * /*pG4Run*/)
+{
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------ 
+
+void G4MCParticleUserAction::BeginOfEventAction(const G4Event * /*pG4Event*/)
+{
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------ 
+
+void G4MCParticleUserAction::EndOfEventAction(const G4Event * /*pG4Event*/)
+{
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------ 
+
+void G4MCParticleUserAction::PreTrackingAction(const G4Track * /*pG4Track*/)
+{
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------ 
+
+void G4MCParticleUserAction::PostTrackingAction(const G4Track * /*pG4Track*/)
+{
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------ 
+
+void G4MCParticleUserAction::SteppingAction(const G4Step * /*pG4Step*/)
+{
+}
+
